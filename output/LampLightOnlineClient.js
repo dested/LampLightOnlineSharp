2ba2d800e00bdc793e048640f862107f4f3af031
Salvatore <dested@gmail.com>
2012-08-30 01:31:15 -0700

    Initial Commit

---
+++ output/LampLightOnlineClient.js
@@ -0,0 +1,8 @@
+
+Type.registerNamespace('LampLightOnlineServer');
+////////////////////////////////////////////////////////////////////////////////
+// LampLightOnlineServer.Class1
+LampLightOnlineServer.Class1 = function() {
+};
+LampLightOnlineServer.Class1.registerClass('LampLightOnlineServer.Class1', Object);
+
